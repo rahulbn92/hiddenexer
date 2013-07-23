75f1908ab7da6f947ae99e4d1246684f3f60b2fb
root <root@vinsol-HP-ProBook-4530s.(none)>
2013-07-24 00:25:21 +0530

    h1

--- jsJSONAutocomplete/jsJSONAutocomplete13.js
+++ jsJSONAutocomplete/jsJSONAutocomplete13.js
@@ -1,31 +1,31 @@
 var inputText = document.getElementById("textBox");
 inputText.onkeyup = function() {
-var jsonObject = [
-  {"name":"Luigi Damiano"},
-  {"name":"Zenith Coboro"},
-  {"name":"Zig Ziglar"},
-  {"name":"Steve Costner"},
-  {"name":"Bill Grazer"},
-  {"name":"Timothy Frazer"},
-  {"name":"Boris Becker"},
-  {"name":"Glenn Gladwich"},
-  {"name":"Jim Jackson"},
-  {"name":"Aaron Kabin"},
-  {"name":"Roy Goldwin"},
-  {"name":"Jason Goldberg"},
-  {"name":"Tim Ferris"},
-  {"name":"Buck Singham"},
-  {"name":"Malcom Gladwell"},
-  {"name":"Joy Rabura"},
-  {"name":"Vid Luther"},
-  {"name":"Tom Glicken"},
-  {"name":"Ray Baxter"},
-  {"name":"Ari Kama"},
-  {"name":"Kenichi Suzuki"},
-  {"name":"Rick Olson"}
-];
-var person = new showSuggestion(jsonObject);
-person.display();
+  var jsonObject = [
+    {"name":"Luigi Damiano"},
+    {"name":"Zenith Coboro"},
+    {"name":"Zig Ziglar"},
+    {"name":"Steve Costner"},
+    {"name":"Bill Grazer"},
+    {"name":"Timothy Frazer"},
+    {"name":"Boris Becker"},
+    {"name":"Glenn Gladwich"},
+    {"name":"Jim Jackson"},
+    {"name":"Aaron Kabin"},
+    {"name":"Roy Goldwin"},
+    {"name":"Jason Goldberg"},
+    {"name":"Tim Ferris"},
+    {"name":"Buck Singham"},
+    {"name":"Malcom Gladwell"},
+    {"name":"Joy Rabura"},
+    {"name":"Vid Luther"},
+    {"name":"Tom Glicken"},
+    {"name":"Ray Baxter"},
+    {"name":"Ari Kama"},
+    {"name":"Kenichi Suzuki"},
+    {"name":"Rick Olson"}
+  ];
+  var person = new showSuggestion(jsonObject);
+  person.display();
 }
 
 
